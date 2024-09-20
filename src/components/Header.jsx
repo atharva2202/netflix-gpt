10c7aa792d6d4a2dfbd2ba78b2b0539a6163f5c4
ABC <phadkeatharva03@gmail.com>
2024-09-20 10:55:03 +0530

    given multilingual support

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO, USER_AVATAR } from "../utils/constants";
+import { LOGO, USER_AVATAR, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   const handleSignout = () => {
     signOut(auth)
       .then(() => {
-        // Sign-out successful.
         navigate("/");
       })
       .catch((error) => {
-        // An error happened.
         navigate("/error");
       });
   };
@@ -28,10 +30,10 @@ const Header = () => {
         const { uid, email, displayName, photoURL } = user;
         dispatch(
           addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL,
+            uid,
+            email,
+            displayName,
+            photoURL,
           })
         );
         navigate("/browse");
@@ -41,16 +43,52 @@ const Header = () => {
       }
     });
 
-    // Unsubscribe when component unmounts
     return () => unsubscribe();
   }, []);
+
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img src={LOGO} alt="logo" />
+    <div className="fixed top-0 left-0 w-full px-8 py-4 bg-black z-20 flex justify-between items-center ">
+      {/* Netflix Logo */}
+      <img src={LOGO} alt="logo" className="mr-8 w-32" />
+
       {user && (
-        <div className="flex p-2">
-          <img src={USER_AVATAR} alt="user-icon" className="w-12 h-12" />
-          <button onClick={handleSignout} className="font-bold text-white">
+        <div className="flex items-center space-x-6">
+          {/* GPT Search Button */}
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="px-4 py-2 bg-purple-800 text-white rounded-lg text-sm"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Home" : "GPT Search"}
+          </button>
+
+          {/* User Avatar */}
+          <img src={USER_AVATAR} alt="user-icon" className="w-12 h-12 " />
+
+          {/* SignOut Button */}
+          <button
+            onClick={handleSignout}
+            className="font-bold text-white text-sm"
+          >
             SignOut
           </button>
         </div>
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -7,7 +7,7 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
       <div className=" bg-black">
-        <div className="-mt-52 pl-12 relative z-20">
+        <div className="-mt-52 pl-10 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
